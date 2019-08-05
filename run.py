090dbf60412c911f9c84710a289831e1b4f24f77
AleksandrRybin <Vfibyrf007@gmail.com>
2019-08-05 23:08:28 +0300

    add args for debug mode and port

--- run.py
+++ run.py
@@ -1,3 +1,21 @@
+# std
+import argparse
+
+# locals
 from src import app
 
-app.run()
+arg_parser = argparse.ArgumentParser(description='Run sentiment analysis demo server')
+
+arg_parser.add_argument('--port', 
+                        dest='port', default=5000, type=int,
+                        help='Port to run'
+)
+
+arg_parser.add_argument('-d', '--debug',
+                        dest='debug', default=False, action='store_true',
+                        help='Run in debug mode or not'
+)
+
+args = arg_parser.parse_args()
+
+app.run(port=args.port, debug=args.debug)
